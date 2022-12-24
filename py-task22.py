049759ad482172b58909aa8a514ce060bea9b198
Alexandr <jwchaganav@gmail.com>
2022-12-24 16:54:11 +0400

    сделал с помощью & пересечения множества

--- py-task22.py
+++ py-task22.py
@@ -1,10 +1,12 @@
 import random
+import os
+os.system('cls')
 
-# n = int(input("Введите кол-во элементов первого набора: "))
-# m = int(input("Введите кол-во элементов второго набора: "))
+n = int(input("Введите кол-во элементов первого набора: "))
+m = int(input("Введите кол-во элементов второго набора: "))
+# n = 6
+# m = 7
 
-n = 6
-m = 7
 min = 1
 max = 6
 nums1 = []
@@ -16,4 +18,8 @@ for num in range(0,m):
     nums2.append(random.randint(min,max))
 print(nums2)
 setNum1 = set(nums1)
-print(setNum1)
+# print(setNum1)
+setNum2 = set(nums2)
+# print(setNum2)
+setNew = setNum1 & setNum2
+print(f"Пересечение: {setNew}")
