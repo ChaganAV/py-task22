0ae4388699f68a262158ec7c517656daa8c3a0ee
Alexandr <jwchaganav@gmail.com>
2022-12-24 18:00:17 +0400

    добавил try

--- py-task22.py
+++ py-task22.py
@@ -9,26 +9,28 @@ def FindIntersect(set1, set2):
         if num in set2:
             nums.append(num)
     return nums
+try:
+    n = int(input("Введите кол-во элементов первого набора: "))
+    m = int(input("Введите кол-во элементов второго набора: "))
+    # n = 6
+    # m = 7
 
-n = int(input("Введите кол-во элементов первого набора: "))
-m = int(input("Введите кол-во элементов второго набора: "))
-# n = 6
-# m = 7
-
-min = 1
-max = 6
-nums1 = []
-nums2 = []
-for num in range(0,n):
-    nums1.append(random.randint(min,max))
-print(nums1)
-for num in range(0,m):
-    nums2.append(random.randint(min,max))
-print(nums2)
-setNum1 = set(nums1)
-# print(setNum1)
-setNum2 = set(nums2)
-# print(setNum2)
-# setNew = setNum1 & setNum2
-setNew = FindIntersect(setNum1, setNum2)
-print(f"Пересечение: {setNew}")
+    min = 1
+    max = 6
+    nums1 = []
+    nums2 = []
+    for num in range(0,n):
+        nums1.append(random.randint(min,max))
+    print(nums1)
+    for num in range(0,m):
+        nums2.append(random.randint(min,max))
+    print(nums2)
+    setNum1 = set(nums1)
+    # print(setNum1)
+    setNum2 = set(nums2)
+    # print(setNum2)
+    # setNew = setNum1 & setNum2
+    setNew = FindIntersect(setNum1, setNum2)
+    print(f"Пересечение: {setNew}")
+except:
+    print("Что-то пошло не так :(")
