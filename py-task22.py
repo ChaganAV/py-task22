359e7940a3672933ad640f921f8c65bcf720a9b5
Alexandr <jwchaganav@gmail.com>
2022-12-24 16:39:00 +0400

    два набора чисел

---
+++ py-task22.py
@@ -0,0 +1,19 @@
+import random
+
+# n = int(input("Введите кол-во элементов первого набора: "))
+# m = int(input("Введите кол-во элементов второго набора: "))
+
+n = 6
+m = 7
+min = 1
+max = 6
+nums1 = []
+nums2 = []
+for num in range(0,n):
+    nums1.append(random.randint(min,max))
+print(nums1)
+for num in range(0,m):
+    nums2.append(random.randint(min,max))
+print(nums2)
+setNum1 = set(nums1)
+print(setNum1)
